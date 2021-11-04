305fba76d14b95f7c7d91b3d7ea89888093d948e
Joao Heberle <joaomheberle@gmail.com>
2021-11-04 16:40:25 -0300

    1

--- aula8/app.js
+++ aula8/app.js
@@ -3,7 +3,7 @@ var express = require("express");
 const app = express();
 const path = require('path');
 
-/* app.set('view engine', 'ejs');
+app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
@@ -13,10 +13,10 @@ app.get('/', (req, res) => {
     res.render('busca');
 });
 
-app.get('/buscaISBN', (req, res) => {
-    const {isbn} = req.query;
+app.get('/buscaCidade', (req, res) => {
+    const {cidade} = req.query;
     var resposta = {};
-    request("https://www.googleapis.com/books/v1/volumes?q=isbn:" + isbn, (error, response, body) => {
+    request("https://api.hgbrasil.com/weather?key=351365a3&city_name=" + cidade, (error, response, body) => {
         if(!error && response.statusCode == 200){
             resposta = JSON.parse(body);
         }
@@ -24,18 +24,7 @@ app.get('/buscaISBN', (req, res) => {
     });
 });
 
-app.get('/buscaTitulo', (req, res) => {
-    const {titulo} = req.query;
-    var resposta = {};
-    request("https://www.googleapis.com/books/v1/volumes?q=intitle:" + titulo, (error, response, body) => {
-        if(!error && response.statusCode == 200){
-            resposta = JSON.parse(body);
-        }
-        res.render('resultadoBusca', {resposta});
-    });
-});
-
-app.listen(3000, () => console.log("Servidor ligado na porta 3000!")); */
+app.listen(3000, () => console.log("Servidor ligado na porta 3000!"));
 
 
 // request("https://api.hgbrasil.com/weather?key=351365a3&city_name=Campinas,SP" , (error, response, body)=>{
